460615cae5a264f20aa94ba17652244039eb13de
Joelio Vilanova <joeliovl@gmail.com>
2019-06-21 16:41:33 -0300

    Versão 1

--- app/src/main/java/praticas/irent/MainActivity.kt
+++ app/src/main/java/praticas/irent/MainActivity.kt
@@ -1,19 +1,36 @@
-package praticas.irent
+package com.example.irentlogin
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
+import praticas.irent.R
+import praticas.irent.RecuperarSenha
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)//will hide the title
+        this.getWindow()
+            .setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener {
-          startActivity<RecuperarSenha>()
+        retrieveText.setOnClickListener(){
+            startActivity<RecuperarSenha>()
         }
     }
 
+    private fun entrar() {
+        val email: EditText = findViewById(R.id.emailText)
+        val senha: EditText = findViewById(R.id.passText)
+        val sb = StringBuilder()
+        //sb.append(email).append(" ").append(senha)
+       // val str = sb.toString()
+       // Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
+    }
 }
